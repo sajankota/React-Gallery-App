8e66c8a90243b094babe36b9d62b56c893993d6a
sajankota <sajankota001@gmail.com>
2023-06-16 04:00:37 +0530

    improved the aligment of the code

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,54 @@ import ImageSearch from "./components/ImageSearch";
 
 function App() {
   const [images, setImages] = useState([]);
-  const [isLoading, setisLoading] = useState(true);
+  const [isLoading, setLoading] = useState(true);
   const [term, setTerm] = useState("");
 
   useEffect(() => {
-    fetch(
-      `https://pixabay.com/api/?key=q=${term}&image_type=photo&pretty=true`
-    )
-      .then((res) => res.json())
-      .then((data) => {
+    const fetchImages = async () => {
+      try {
+        const response = await fetch(
+          `https://pixabay.com/api/?key=763927-16a142039b5d7cb559c16207e&q=${term}&image_type=photo&pretty=true`
+        );
+        const data = await response.json();
         setImages(data.hits);
-        setisLoading(false);
-      })
-      .catch((error) => console.log(error));
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchImages();
   }, [term]);
 
   return (
-    <>
-      <div className="container mx-auto">
-        <ImageSearch searchText={(text) => setTerm(text)} />
-        {!isLoading && images.length === 0 && (
-          <h1 className="text-5xl text-center mx-auto">no images found</h1>
-        )}
-        {isLoading ? (
-          <h1 className="text-6xl text-center mx-auto">Loading...</h1>
+    <div className="min-h-screen flex items-center">
+      <div className="container mx-auto px-4">
+        <ImageSearch searchText={setTerm} />
+        {!isLoading && images.length === 0 ? (
+          <h1 className="text-5xl text-center">No images found</h1>
         ) : (
-          <div className="grid grid-cols-3 gap-4">
-            {images.map((image) => (
-              <ImageCard key={image.id} image={image} />
-            ))}
-          </div>
+          <>
+            {isLoading ? (
+              <h1 className="text-6xl text-center">Loading...</h1>
+            ) : (
+              <div className="max-w-screen-xl mx-auto grid grid-cols-3 gap-6">
+                {images.map((image, index) => (
+                  <div
+                    key={image.id}
+                    className={`${
+                      index === 0 ? "ml-6" : ""
+                    } flex justify-center`}
+                  >
+                    <ImageCard image={image} />
+                  </div>
+                ))}
+              </div>
+            )}
+          </>
         )}
       </div>
-    </>
+    </div>
   );
 }
 
