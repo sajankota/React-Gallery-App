03769f92456dcc881b6bd6aa92247b664e8f1861
sajankota <sajankota001@gmail.com>
2023-06-16 10:37:01 +0530

    optimized the code and added detailed comments for ImageCard and ImageSearch components

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,38 +1,56 @@
+// Import React to use JSX syntax
+import React from "react";
+
+// Define a functional component called ImageCard that takes the 'image' prop
 const ImageCard = ({ image }) => {
+  // Split the 'tags' string into an array using commas as separators
   const tags = image.tags.split(",");
+
+  // Return the JSX structure representing the image card component
   return (
-    <div className="max-w-sm round overflow-hidden shadow-lg">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      {/* Display the image using the 'webformatURL' from the 'image' object */}
       <img src={image.webformatURL} alt="Random Image" className="w-full" />
+
       <div className="px-6 py-4">
-        <div className=".font-bold.text.purple-500.text-xl.mb-2">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          {/* Display the username of the image author */}
           Photo by {image.user}
         </div>
+
         <ul>
           <li>
             <strong>Views:</strong>
+            {/* Display the number of views of the image */}
             {image.views}
           </li>
           <li>
             <strong>Downloads:</strong>
+            {/* Display the number of downloads of the image */}
             {image.downloads}
           </li>
           <li>
             <strong>Likes:</strong>
+            {/* Display the number of likes of the image */}
             {image.likes}
           </li>
         </ul>
       </div>
+
       <div className="px-6 py-4">
+        {/* Iterate over the 'tags' array and display each tag */}
         {tags.map((tag, index) => (
           <span
             key={index}
             className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
           >
-            #{tag}
+            {/* Display each tag preceded by '#' */}#{tag}
           </span>
         ))}
       </div>
     </div>
   );
 };
+
+// Export the ImageCard component as the default export
 export default ImageCard;
--- src/components/ImageSearch.jsx
+++ src/components/ImageSearch.jsx
@@ -1,24 +1,38 @@
-import { useState } from "react";
+// Import React and useState hook from the 'react' package
+import React, { useState } from "react";
 
+// Define a functional component called ImageSearch that takes the 'searchText' prop
 const ImageSearch = ({ searchText }) => {
-  const [text, setText] = useState();
+  // Declare a state variable 'text' using the useState hook and set its initial value to an empty string
+  const [text, setText] = useState("");
+
+  // Define an event handler function 'onSubmit' that is triggered when the form is submitted
   const onSubmit = (e) => {
+    // Prevent the default form submission behavior, which would cause a page reload
     e.preventDefault();
+
+    // Invoke the 'searchText' function passed as a prop and pass the current value of 'text' as an argument
     searchText(text);
   };
+
+  // Return the JSX structure representing the image search component
   return (
     <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
       <form onSubmit={onSubmit} className="w-full max-w-sm">
         <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
+          {/* Input field for entering the search term */}
           <input
-            onChange={(e) => setText(e.target.value)}
+            onChange={(e) => setText(e.target.value)} // Update the 'text' state with the current input value
+            value={text} // Bind the value of the input field to the 'text' state
             className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
             type="text"
             placeholder="Search Image Term..."
           />
+
+          {/* Submit button */}
           <button
-            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
             type="submit"
+            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm text-white py-1 px-2 rounded"
           >
             Search
           </button>
@@ -28,4 +42,5 @@ const ImageSearch = ({ searchText }) => {
   );
 };
 
+// Export the ImageSearch component as the default export
 export default ImageSearch;
